66dac67beafd7c765e1363ab30f9d4d8b061bba0
Oliver Säfström <oliver.safstrom@mobilityguard.com>
2025-07-01 07:27:04 +0200

    Fix clipboard issue

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,15 +1,29 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
 vim.opt.rtp:prepend(lazypath)
 
+-- Support for remote clipboard ssh
+vim.g.clipboard = {
+  name = 'OSC 52',
+  copy = {
+    ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+    ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+  },
+  paste = {
+    ['+'] = function() return { vim.fn.split(vim.fn.getreg(''), '\\n'), vim.fn.getregtype('') } end,
+    ['*'] = function() return { vim.fn.split(vim.fn.getreg('*'), '\\n'), vim.fn.getregtype('*') } end,
+  },
+}
+
+vim.opt.clipboard:append { 'unnamed', 'unnamedplus' }
 require("config")
 require("lazy").setup("plugins")
